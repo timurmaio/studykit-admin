8373f457df2c38e0ffd5dd07a90e5a7c74a6f4f4
Тимур Макаров <tmakarov95@gmail.com>
2017-04-13 18:19:25 +0300

    remove(App): constructor

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {}
-  }
-
   render () {
     return (
       <div>
